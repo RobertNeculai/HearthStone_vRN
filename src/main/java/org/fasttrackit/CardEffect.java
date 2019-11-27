761a10b154f4604a6f98e17fe6717707fc981766
Neculai Robert <robert.neculai13@gmail.com>
2019-11-27 18:27:22 +0200

    Adaugarea proprietatii Taunt la Card Effect

--- src/main/java/org/fasttrackit/CardEffect.java
+++ src/main/java/org/fasttrackit/CardEffect.java
@@ -6,12 +6,14 @@ public class CardEffect {
     int attack;
     int defense;
     int heal;
+    boolean taunt;
 
-    public CardEffect(String efect, int cost, int attack, int defense, int heal) {
+    public CardEffect(String efect, int cost, int attack, int defense, int heal,boolean taunt) {
         this.efect = efect;
         this.cost = cost;
         this.attack = attack;
         this.defense = defense;
         this.heal = heal;
+        this.taunt=taunt;
     }
 }
--- src/main/java/org/fasttrackit/Turn.java
+++ src/main/java/org/fasttrackit/Turn.java
@@ -6,4 +6,6 @@ public class Turn
     int mana;
     int tnumber;
     int healthRemaining;
+    // healthRemaining se refera la viata eroului care porneste de la 30 inceputul meciului si care poate varia in functie de modul de match ales
+    // cartile pot ataca eroii adversari,cat timp nu exista o carte care sa interfereze cu aceasta actiune
 }
